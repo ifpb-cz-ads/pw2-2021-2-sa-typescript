6855c7948cfee3bfc4dcc39eefa1b91180260d62
Wendel Alves <wendel.alves@academico.ifpb.edu.br>
2022-03-26 13:23:52 -0300

    Adicionando projeto

---
+++ index.ts
@@ -0,0 +1,38 @@
+
+//let valor = 10;
+//let string: string = 'Wendel';
+//let array: Array<string> = ['1', '2']
+
+//console.log(valor, string, array)
+
+//function imprimir (nome: string, sobrenome: string, idade: number) {
+//    console.log({nome, sobrenome, idade})
+//}
+
+
+//type Pessoa = {
+//    nome: string;
+//    sobrenome: string;
+//    idade: number
+//}
+
+//interface Pessoa {
+//    nome: string;
+//    sobrenome: string;
+//    idade: number
+//}
+//
+//function teste ({idade, nome, sobrenome}:Pessoa) {
+//    console.log({nome, sobrenome, idade});
+//}
+//
+//teste({nome: 'Wendel', idade: 21, sobrenome: 'Alves'})
+
+let x: number = 10;
+
+let string: string = x;
+
+
+
+
+
